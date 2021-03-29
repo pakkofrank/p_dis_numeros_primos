1c1e368be5d787b40215f40edb3b41a67f0f6dc8
pakkofrank <jpalma@ucol.mx>
2021-03-28 22:28:46 -0600

    creo que ya quedo

--- src/main/java/mx/ucol/threads/App.java
+++ src/main/java/mx/ucol/threads/App.java
@@ -2,10 +2,22 @@ package mx.ucol.threads;
 
 public class App {
     private final static int MAX = 5_000_000;
-
     // TODO
     // Create a nested class that uses the countPrimes() method and implements Runnable
     // HINT: You can use System.currentTimeMillis() to capture the current time of the system
+
+    public  static  class tClass implements Runnable{
+        @Override
+
+        public void run(){
+            float inicial = System.currentTimeMillis();
+            float fin = System.currentTimeMillis();
+            int total = countPrimes(1, MAX);
+            float diferencia = fin - inicial;
+            float segundos = diferencia / 100;
+            System.out.println(Thread.currentThread().getName() + "contado " + total + " primos" + segundos + " segundos");
+        }
+    }
 
     public static void main(String args[]) throws InterruptedException {
         int numberOfThreads = 0;
@@ -27,6 +39,12 @@ public class App {
         // TODO
         // Create threads (HINT: You can store threads in arrays)
         // HINT: You can store threads in arrays
+
+        tClass threadsArray[] = new tClass[numberOfThreads];
+        for (int i=0; i < numberOfThreads; i++){
+            threadsArray[i] = new tClass();
+            threadsArray[i].run();
+        }
 
    }
 
